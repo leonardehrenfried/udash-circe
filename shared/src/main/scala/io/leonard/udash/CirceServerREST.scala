1ab796cb4f5a1809d235bb7f5459e6b9864c62ba
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2017-10-20 16:40:19 +0200

    Change move trait into separate file

--- shared/src/main/scala/io/leonard/udash/CirceServerREST.scala
+++ shared/src/main/scala/io/leonard/udash/CirceServerREST.scala
@@ -1,33 +1,9 @@
 package io.leonard.udash
 
-import io.udash.rest.{DefaultRESTConnector, UdashRESTFramework}
+import io.udash.rest.DefaultRESTConnector
 import io.udash.rest.internal.{RESTConnector, UsesREST}
-import io.udash.rpc.serialization.URLEncoder
 
 import scala.concurrent.ExecutionContext
-
-trait RESTConverters {
-  val framework: UdashRESTFramework
-
-  def rawToHeaderArgument(raw: framework.RawValue): String =
-    stripQuotes(framework.rawToString(raw))
-  def rawToQueryArgument(raw: framework.RawValue): String =
-    stripQuotes(framework.rawToString(raw))
-  def rawToURLPart(raw: framework.RawValue): String =
-    stripQuotes(framework.rawToString(raw))
-
-  private def stripQuotes(s: String): String =
-    s.stripPrefix("\"").stripSuffix("\"")
-
-  def headerArgumentToRaw(raw: String, isStringArg: Boolean): framework.RawValue = rawArg(raw, isStringArg)
-  def queryArgumentToRaw(raw: String, isStringArg: Boolean): framework.RawValue  = rawArg(raw, isStringArg)
-  def urlPartToRaw(raw: String, isStringArg: Boolean): framework.RawValue =
-    rawArg(URLEncoder.decode(raw), isStringArg)
-
-  private def rawArg(raw: String, isStringArg: Boolean): framework.RawValue =
-    if (isStringArg) framework.stringToRaw(s""""$raw"""")
-    else framework.stringToRaw(raw)
-}
 
 /** Default REST usage mechanism using [[io.leonard.udash.CirceServerREST]]. */
 class CirceServerREST[
